ea40d703c6ca47293967cab52fb82b0b700c9e91
Alexander Li <43350164+Hermanubis@users.noreply.github.com>
2021-11-18 15:23:43 -0500

    update

---
+++ Project2/app/src/main/java/edu/gwu/project2/resultActivity.kt
@@ -0,0 +1,14 @@
+package edu.gwu.project2
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.anko.doAsync
+
+class resultActivity : AppCompatActivity() {
+}
