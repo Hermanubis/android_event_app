193dc48217a14b40364e6bb501445f7dd2161ffd
Alexander Li <43350164+Hermanubis@users.noreply.github.com>
2021-11-19 21:26:53 -0500

    update

--- Project2/app/src/main/java/edu/gwu/project2/mapboxActivity.kt
+++ Project2/app/src/main/java/edu/gwu/project2/mapboxActivity.kt
@@ -1,4 +1,63 @@
 package edu.gwu.project2
 
-class mapboxActivity {
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.location.Address
+import android.location.Geocoder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.mapbox.mapboxsdk.*
+
+import com.google.android.material.button.MaterialButton
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+import org.jetbrains.anko.doAsync
+
+
+
+class mapboxActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
+        setContentView(R.layout.activity_mapbox)
+        mapView = findViewById (R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync{ mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS){ style ->
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
 }
